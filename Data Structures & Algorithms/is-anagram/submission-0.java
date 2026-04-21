a12f345fd1b984f8d3362fcd7f6f8dd1205d0f35
Airrishkhan <airrishaws24@gmail.com>
2026-04-21 12:40:23 +0530

    Add: is-anagram - submission-0

---
+++ Data Structures & Algorithms/is-anagram/submission-0.java
@@ -0,0 +1,36 @@
+class Solution {
+    public boolean isAnagram(String s, String t) {
+
+        Hashtable<Character,Integer> ht1 = new Hashtable<>();
+        Hashtable<Character,Integer> ht2 = new Hashtable<>();
+        int counter = 0 ; 
+
+        for(int i = 0 ; i < s.length() ; i++) {
+
+            if(ht1.containsKey(s.charAt(i))==false) { 
+                ht1.put(s.charAt(i),counter);
+            }else {
+                ht1.put(s.charAt(i),ht1.get(s.charAt(i))+1);
+            }
+
+        }
+
+        for(int i = 0 ; i < t.length() ; i++) {
+
+            if(ht2.containsKey(t.charAt(i))==false) { 
+                ht2.put(t.charAt(i),counter);
+            }else {
+                ht2.put(t.charAt(i),ht2.get(t.charAt(i))+1);
+            }
+
+        }
+
+        if(ht1.entrySet().equals(ht2.entrySet())){
+            return true;
+        }else {
+            return false;
+        }
+
+
+    }
+}
