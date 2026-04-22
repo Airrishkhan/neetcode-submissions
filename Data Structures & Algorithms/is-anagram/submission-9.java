cffaf0431993020f99844b5f746fa9808cc1f08b
Airrishkhan <airrishaws24@gmail.com>
2026-04-22 12:21:44 +0530

    Add: is-anagram - submission-9

---
+++ Data Structures & Algorithms/is-anagram/submission-9.java
@@ -0,0 +1,39 @@
+class Solution {
+    public boolean isAnagram(String s, String t) {
+
+        HashMap<Character,Integer> hm = new HashMap<>();
+
+        if(s.length() != t.length()) {
+            return false;
+        }
+
+        int counter = 1;
+        //put in hashmap
+        for(int i=0 ; i<s.length() ; i++) {
+            
+            if(hm.containsKey(s.charAt(i))==false){
+                hm.put(s.charAt(i),counter);
+            }else {
+                hm.put(s.charAt(i),hm.get(s.charAt(i))+1);
+            }
+
+        }
+
+        for(int i=0 ; i<t.length() ; i++) {
+
+            if(hm.containsKey(t.charAt(i))==false) {
+                return false;
+            }else if(hm.get(t.charAt(i)) > 0) {
+                    hm.put(t.charAt(i),hm.get(t.charAt(i))-1);
+            }else {
+                return false;
+            }
+
+        }
+
+        return true;
+   
+    }
+
+}
+
